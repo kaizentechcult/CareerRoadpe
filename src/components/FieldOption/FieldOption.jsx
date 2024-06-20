e695da6c70408c41ca452275f651d69490923be3
KRISH KHANNA <krishkhanna00@gmail.com>
2024-06-21 00:39:18 +0530

    made progress on the fieldsMenu page

--- src/components/FieldOption/FieldOption.jsx
+++ src/components/FieldOption/FieldOption.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function FieldOption({ data }) {
-  console.log(data);
+  //   console.log(data);
   return (
     <>
-      <Link to={`/roadmap/${data}`}>
-        <div>this is the option</div>
+      <Link to={`/roadmap/${data.title}`}>
+        <div className="flex relative bg-gray-900 px-5 py-3 rounded-lg">
+          <img
+            className="absolute top-1 left-[4px] h-[40px] w-[20px]"
+            src="bookmark.svg"
+          />
+          <p className="px-3">{data.title}</p>
+        </div>
       </Link>
     </>
   );
