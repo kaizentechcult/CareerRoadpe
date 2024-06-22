689d10f799da3d968065451cddcc5828a9b5266d
Anurag Raj <154009382+Anurag07-07@users.noreply.github.com>
2024-06-22 16:29:16 +0530

    Added desktop

--- src/pages/FieldMenu/FieldMenu.jsx
+++ src/pages/FieldMenu/FieldMenu.jsx
@@ -2,10 +2,10 @@ import FieldOption from "../../components/FieldOption/FieldOption";
 function Fields({ data }) {
   // console.log(data);
   return (
-    <div className="flex justify-center">
-      <ul className="flex flex-col gap-2 list-style-none">
+    <div className="flex justify-center md:mt-52 md:h-max">
+      <ul className="flex flex-col gap-2 list-style-none md:grid md:grid-cols-2 md:gap-x-24">
         {data.map((item) => (
-          <li key={item.id} className="inline-block mr-4 mb-4">
+          <li key={item.id} className="inline-block mr-4 mb-4 md:w-[30vw]">
             <FieldOption data={item} />
           </li>
         ))}
