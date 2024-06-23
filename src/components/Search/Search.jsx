abf3fba0ed3dabf68c3ca104ee57100a355f93bc
Sooraj <soorajnambiar8@gmail.com>
2024-06-23 15:38:48 +0530

    added btn

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,14 @@
+import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
   return (
     <div className="flex flex-col justify-center items-center pt-16 gap-6">
-      <input type="text" className="p-6 rounded-full w-1/2 text-black" />
+      <form className="flex w-1/2">
+        <button className=""> 
+          <FaSearch  className="scale-[2] absolute right-[26rem] bg-blue-600 p-4 rounded-full text-[#f1ab4f] "/>
+        </button>
+        <input type="text" className="p-6 rounded-full w-full text-black" />
+      </form>
       <p className="text-5xl font-semibold text-center">Search Roadmap</p>
     </div>
   );
