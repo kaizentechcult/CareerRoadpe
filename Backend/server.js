5f1bafea149cb9a1045b6a0dc308d54fd8a85948
Sooraj <soorajnambiar8@gmail.com>
2024-06-26 18:48:49 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const app = express();
 var hivesigner = require("hivesigner");
+var client = new hivesigner.Client({
+  app: "demo",
+  callbackURL: "https://demo.hivesigner.com",
+  scope: ["vote", "comment"],
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-var client = new hivesigner.Client({
-  app: 'demo',
-  callbackURL: 'https://demo.hivesigner.com',
-  scope: ['vote', 'comment']
-});
+app.post("/signup", (req, res)=>{
+  console.log(req.body);
+})
 
 client.login("params", "callbackURL");
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("Sever is listening to port 8080");
+});
