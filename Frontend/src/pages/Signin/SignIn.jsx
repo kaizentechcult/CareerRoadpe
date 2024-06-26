3ffe07b0aa5b51db1b38bc91dfce4cf67d70353d
Sooraj <soorajnambiar8@gmail.com>
2024-06-26 22:32:06 +0530

    added build

--- Frontend/src/pages/Signin/SignIn.jsx
+++ Frontend/src/pages/Signin/SignIn.jsx
@@ -1,20 +1,43 @@
+import React, { useState } from "react";
 import Input from "../../components/Input/Input";
+// import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  useState();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  console.log(username, password);
   return (
     <div className="flex flex-col items-center h-screen">
-      <div className="flex flex-col justify-center items-center p-8 gap-6 bg-[#212020] rounded-xl py-20">
+      <div className="flex flex-col justify-center items-center p-4 gap-6 bg-[#212020] rounded-xl py-10 w-5/6 md:w-[25%]">
         <form
           action=""
           className="text-white text-center flex flex-col gap-6 w-5/6"
         >
           <h1 className="text-2xl font-semibold">Sign In</h1>
-          <Input name="email" id="email" placeholder="Enter your email" />
+          <Input
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            value={username}
+            onchange={handleUsernameChange}
+          />
           <Input
             name="password"
             id="password"
             placeholder="Enter your password"
+            value={password}
+            onchange={handlePasswordChange}
           />
           <button
             className="bg-[#4F7EC3] hover:text-white rounded-3xl px-6 py-3"
--- Frontend/src/pages/Signup/SignUp.jsx
+++ Frontend/src/pages/Signup/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SignUp = () => {
   return (
     <div className="flex flex-col items-center h-screen">
-      <div className="flex flex-col justify-center items-center p-8 gap-6 bg-[#212020] rounded-xl w-5/6 md:w-1/3 py-20">
+      <div className="flex flex-col justify-center items-center p-4 gap-6 bg-[#212020] rounded-xl w-5/6 md:w-[25%] py-10">
         <form
           action=""
           className="text-white text-center flex flex-col gap-6 w-5/6"
