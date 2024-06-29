a84dd37b417a6cd5306c3f348fba4f3e5b8821f0
Sooraj <soorajnambiar8@gmail.com>
2024-06-29 15:49:32 +0530

    added team

---
+++ src/assets/team.js
@@ -0,0 +1,76 @@
+import image1 from "/Team/Adista.jpg"
+import image2 from "/Team/Aditya.jpg"
+import image3 from "/Team/Avinash.jpg"
+import image4 from "/Team/Ayush.jpg"
+import image5 from "/Team/Kanak.jpg"
+import image6 from "/Team/Krish.jpg"
+import image7 from "/Team/Manisha.jpg"
+import image8 from "/Team/Piyush.jpg"
+import image9 from "/Team/Shivansh.jpg"
+import image10 from "/Team/Shree.jpg"
+
+
+const people = [
+  {
+    name: "Ada Lovelace",
+    img: image1,
+    link: "https://en.wikipedia.org/wiki/Ada_Lovelace",
+    profession: "Mathematician & Computer Programmer",
+  },
+  {
+    name: "Marie Curie",
+    img: image2,
+    link: "https://en.wikipedia.org/wiki/Marie_Curie",
+    profession: "Physicist & Chemist",
+  },
+  {
+    name: "Alan Turing",
+    img:image3,
+    link: "https://en.wikipedia.org/wiki/Alan_Turing",
+    profession: "Computer Scientist & Mathematician",
+  },
+  {
+    name: "Tim Berners-Lee",
+    img: image4,
+    link: "https://en.wikipedia.org/wiki/Tim_Berners-Lee",
+    profession: "Computer Scientist & Inventor of World Wide Web",
+  },
+  {
+    name: "Grace Hopper",
+    img: image5,
+    link: "https://en.wikipedia.org/wiki/Grace_Hopper",
+    profession: "Computer Scientist & U.S. Navy Rear Admiral",
+  },
+  {
+    name: "Stephen Hawking",
+    img: image6,
+    link: "https://en.wikipedia.org/wiki/Stephen_Hawking",
+    profession: "Theoretical Physicist & Cosmologist",
+  },
+  {
+    name: "Marie Kondo",
+    img: image7,
+    link: "https://konmari.com/en",
+    profession: "Tidying Expert & Author",
+  },
+  {
+    name: "Elon Musk",
+    img: image8,
+    link: "https://en.wikipedia.org/wiki/Elon_Musk",
+    profession: "Entrepreneur & Business Magnate",
+  },
+  {
+    name: "Oprah Winfrey",
+    img: image9,
+    link: "https://en.wikipedia.org/wiki/Oprah_Winfrey",
+    profession: "Talk Show Host & Media Proprietor",
+  },
+  {
+    name: "Oprah Winfrey",
+    img: image10,
+    link: "https://en.wikipedia.org/wiki/Oprah_Winfrey",
+    profession: "Talk Show Host & Media Proprietor",
+  },
+];
+
+export default people;
--- src/pages/MentorPage/MentorPage.jsx
+++ src/pages/MentorPage/MentorPage.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import team from "../../assets/team";
+
 const MentorPage = () => {
   return (
     <div>
-      <h1 className="text-white">This is MentorPage</h1>
+      <h1 className="text-white text-center text-4xl font-bold p-4">Our Mentors</h1>
+      <div className="flex gap-5 flex-wrap justify-center items-center">
+        {team.map((member) => (
+          <div
+            className="rounded-xl bg-gray-800 text-white p-4 flex gap-8 w-1/3"
+            key={member.id}
+          >
+            <div>
+              <img
+                src={member.img}
+                alt="member"
+                className="w-[7rem] h-[7rem] rounded-full"
+              />
+            </div>
+            <div className="w-1/2 flex justify-evenly flex-col">
+              <p className="font-bold">{member.name}</p>
+              <p>{member.profession}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
